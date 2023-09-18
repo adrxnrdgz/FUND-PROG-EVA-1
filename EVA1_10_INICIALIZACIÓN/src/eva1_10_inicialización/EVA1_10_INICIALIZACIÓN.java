99484de381a00d8a8bd626396107e9c19c713ce9
adrxnrdgz <143474113+adrxnrdgz@users.noreply.github.com>
2023-09-18 16:51:15 -0700

    Add files via upload

---
+++ EVA1_10_INICIALIZACIÓN/src/eva1_10_inicialización/EVA1_10_INICIALIZACIÓN.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_10_inicialización;
+
+/**
+ *
+ * @author angel
+ */
+public class EVA1_10_INICIALIZACIÓN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        int valor = 0;  //INICIALIZANDO LA VARIABLE
+        System.out.println(valor); 
+        
+        int precio; //DECLARAR
+        precio = 100; //INICIALIZANDO
+        
+        double salario = 0, precioMayoreo = 100, iva = 10.5, subottal = 15, total = 100.0; //TENEMOS 5 VARIABLES, TODAS, DE TIPO DOUBLE
+        
+        //CONSTANTES
+        //final ---> TE PERMITE ASIGNAIR UN VALOR SOLO UNA VEZ
+        final int conteo;
+        conteo = 100;
+        System.out.println(conteo);
+       
+        final double MI_VALOR_PI = 3.1416;
+        final String ESCUELA = "Instituto Tecnológico de Chihuahua II";
+        final int CALIFA_PASAR = 70;
+        
+       /*
+        Varias lineas de comentarios 
+        que se van a ignorar
+       */
+        
+       int x; //COMENTARIO DE UNA SOLA LINEA, TODO EN LA LINEA ES UN COMENTARIO
+       
+    }
+    
+}
